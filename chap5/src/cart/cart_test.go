ba403307b799122cf272b08f2e3af58e181b17e9
Satoshi Nagayasu <snaga@uptime.jp>
2018-10-03 10:11:14 +0900

    基本的なユニットテスト

---
+++ chap5/src/cart/cart_test.go
@@ -0,0 +1,23 @@
+package cart
+
+import "testing"
+
+func TestAddAndGetProductsInCart(t *testing.T) {
+    c := New()
+    c.Add("りんご")
+    c.Add("みかん")
+//    c.Add("バナナ")
+
+    products := c.GetAll()
+    if len(products) != 2 {
+        t.Fatalf("商品の数が違う %d", len(products))
+    }
+    if products[0] != "りんご" && products[1] != "りんご" {
+        t.Error("りんご not found.")
+        t.Log("カートの中身: ", products)
+    }
+    if products[0] != "みかん" && products[1] != "みかん" {
+        t.Error("みかん not found.")
+        t.Log("カートの中身: ", products)
+    }
+}
